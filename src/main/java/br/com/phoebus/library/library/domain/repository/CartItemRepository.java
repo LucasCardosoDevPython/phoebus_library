b3289dfa1b062821e8d99319a6d7282a5ab3a69a
Lucas Cardoso <101292926+LucasCardosoDevPython@users.noreply.github.com>
2022-09-21 08:49:19 -0300

    Delete src/main/java/br/com/phoebus/library/library/domain directory

--- src/main/java/br/com/phoebus/library/library/domain/repository/CartItemRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.phoebus.library.library.domain.repository;
-
-import br.com.phoebus.library.library.domain.entity.CartItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartItemRepository extends JpaRepository<CartItem, Integer> {
-}
